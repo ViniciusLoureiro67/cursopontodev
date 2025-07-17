27ec7f008f9e84978868157f67e5d8eee6a16520
Vini.Cyber <viniciussloureiro@hotmail.com>
2025-07-17 16:42:58 -0300

    just updating details on neon db and chaing domain name

--- infra/database.js
+++ infra/database.js
@@ -1,6 +1,3 @@
-import dotenv from "dotenv";
-dotenv.config();
-
 import { Client } from "pg";
 
 async function query(queryObject) {
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from "infra/database.js";
+import database from "infra/database";
 
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
@@ -17,8 +17,6 @@ async function status(request, response) {
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
     values: [databaseName],
   });
-
-  // "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
   const databaseOpenedConnectionsValue =
     databaseOpenedConnectionsResult.rows[0].count;
 
